ce6162d6f70dc06d530580976489aff8a1185cf0
Gerson Esquembri Moreno <gersonesmo@uoc.edu>
2020-01-06 20:07:22 +0100

    Finished both pages and @apply.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      lineHeight: {
+        tight: 0.1,
+      },
+      colors: {
+        'body-blue': '#1c3485',
+        'custom-orange': '#ff7b57',
+        'custom-green': '#beff70'
+      }
+    }
   },
   variants: {},
   plugins: []
